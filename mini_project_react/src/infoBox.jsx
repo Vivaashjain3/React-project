2fb5926c8adb4ffbac9ccc684e6d9a54b9131a3e
Vivaash Jain <vivaashjain2@gmail.com>
2024-03-15 01:20:25 +0530

    React

---
+++ mini_project_react/src/infoBox.jsx
@@ -0,0 +1,37 @@
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import './infoBox.css'
+
+
+export default function InfoBox({info}) {
+    const INIT_URL="https://images.unsplash.com/photo-1572687413625-cb2c4d9c4d32?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fGR1c3R5JTIwd2VhdGhlcnxlbnwwfHwwfHx8MA%3D%3D";
+
+    return(
+        <div className="InfoBox">
+            <div className='cardContainer'>
+        <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        sx={{ height: 140 }}
+        image={INIT_URL}
+        title="green iguana"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {info.city}
+        </Typography>
+        <Typography variant="body2" color="text.secondary" component={"span"}>
+          <p>Temperature={info.temp}&deg;C</p>
+          <p>Humidity={info.humidity}</p>
+          <p>Min Temp={info.tempMin}</p>
+          <p>Max Temp={info.tempMax}</p>
+          <p>Feels like={info.feelslike}</p>
+          
+        </Typography>
+      </CardContent>
+    </Card>
+    </div>
+        </div>
+    )
+}
